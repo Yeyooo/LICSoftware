65c0b49ccef95c5fbf31aa7b68f92660d5a7bd9e
Yeyooo <yeyo.cs@gmail.com>
2016-10-06 22:02:27 -0300

    Inicio
    Se da por iniciada la implementación de LICSoftware

---
+++ Main/Main/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Se da por iniciada la implementacion de LICSoftware");
+            Console.ReadKey();
+        }
+    }
+}
