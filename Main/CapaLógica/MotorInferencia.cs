97aec4b359a288dd9a5b28258753f3f360db1e64
Yeyooo <yeyo.cs@gmail.com>
2016-11-12 17:41:42 -0300

    Algoritmo

--- Main/CapaLógica/MotorInferencia.cs
+++ Main/CapaLógica/MotorInferencia.cs
@@ -10,5 +10,34 @@ namespace CapaLógica
     //Clase exclusiva para funciones del motor de inferencia.
     public class MotorInferencia
     {
+
+        función ENCADENAMIENTO-HACIA-DELANTE(KB, c)
+        devuelve T o F
+            # Nº de símbolos en cada cláusula.
+            cuenta ← CREAR-DICCIONARIO()
+            # Si un símbolo ha sido inferido ya.
+            inferido ← CREAR-DICCIONARIO()
+            # Símbolos que se sabe que son verdaderos.
+            agenda ← CREAR-FIFO()
+            por cada cláusula en KB hacer
+                cantidad ← 0
+                por cada símbolo en cláusula.PREMISA hacer
+                    cantidad = cantidad + 1
+                    inferido[símbolo] = F
+                    si símbolo = T entonces agenda.AGREGAR(símbolo)
+                cuenta[cláusula] ← cantidad
+
+            mientras no agenda.ESTÁ-VACÍA() hacer
+                p ← agenda.POP()
+                si p = c entonces devolver T
+                # Evitar repetir y bucles.
+                si inferido[p] = F entonces
+                    inferido[p] ← T
+                    por cada cláusula en KB hacer
+                        si p está en cláusula.PREMISA hacer
+                            cuenta[cláusula] ← cuenta[cláusula] – 1
+                            si cuenta[cláusula] = 0 entonces
+                                agenda.AGREGAR(cláusula.CONCLUSIÓN)
+            devolver F
     }
 }
